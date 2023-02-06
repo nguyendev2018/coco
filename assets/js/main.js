d69d084686a555e5803e69ffd46a835b69695871
nguyendev2018 <nguyendev2018@gmail.com>
2023-02-06 00:59:13 -0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".c-design__bannerlist").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: $(".c-design__arrowright "),
+    prevArrow: $(".c-design__arrowleft"),
+  });
+});
